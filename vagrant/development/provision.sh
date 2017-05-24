bda232c3cc44215a9f98f105d21042399ece55d5
Dag Heyman <dag@yubico.com>
2017-05-24 10:02:38 +0200

    vagrant: add py3 deps to dev env

--- vagrant/development/provision.sh
+++ vagrant/development/provision.sh
@@ -7,7 +7,10 @@ sudo add-apt-repository -y ppa:yubico/stable
 sudo apt-get update -qq && apt-get -qq upgrade
 sudo apt-get install -qq \
     python-pip \
+    python3-pip \
     python-pyscard \
+    python3-pyscard \
+    python3-cryptography \
     python-cryptography \
     libykpers-1-1 \
     libu2f-host0 \
@@ -16,12 +19,13 @@ sudo apt-get install -qq \
     pcscd \
     libffi-dev
 pip install --upgrade pip
+pip3 install --upgrade pip
 
 # Install flake8 for linting
 pip install pre-commit flake8
 
 # Install editable version of repository, install pre-commit hook
-cd /vagrant && pip install -e . && chown -R ubuntu . && pre-commit install
+cd /vagrant && chown -R ubuntu . && pip install -e . && pre-commit install
 
 # Add a very permissive udev rule to be able to access YubiKey Slots over ssh
 echo 'ATTRS{idVendor}=="1050", MODE="0777"' > /etc/udev/rules.d/99-yubico.rules
