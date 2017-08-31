8626c37720cfbe2065639c4db1535764a149ffc5
Dag Heyman <dag@yubico.com>
2017-08-31 10:24:22 +0200

    tests: fix oath tests

--- test/test_oath.py
+++ test/test_oath.py
@@ -5,47 +5,47 @@ import unittest
 class TestOathFunctions(unittest.TestCase):
 
     def test_credential_parse_period_and_issuer_and_name(self):
-        period, issuer, name = Credential.parse_name('20/Issuer:name')
+        period, issuer, name = Credential.parse_long_name('20/Issuer:name')
         self.assertEqual(20, period)
         self.assertEqual('Issuer', issuer)
         self.assertEqual('name', name)
 
     def test_credential_parse_wierd_issuer_and_name(self):
-        period, issuer, name = Credential.parse_name('wierd/Issuer:name')
+        period, issuer, name = Credential.parse_long_name('wierd/Issuer:name')
         self.assertEqual(30, period)
         self.assertEqual('wierd/Issuer', issuer)
         self.assertEqual('name', name)
 
     def test_credential_parse_issuer_and_name(self):
-        period, issuer, name = Credential.parse_name('Issuer:name')
+        period, issuer, name = Credential.parse_long_name('Issuer:name')
         self.assertEqual(30, period)
         self.assertEqual('Issuer', issuer)
         self.assertEqual('name', name)
 
     def test_credential_parse_period_and_name(self):
-        period, issuer, name = Credential.parse_name('20/name')
+        period, issuer, name = Credential.parse_long_name('20/name')
         self.assertEqual(20, period)
         self.assertEqual(None, issuer)
         self.assertEqual('name', name)
 
     def test_credential_parse_only_name(self):
-        period, issuer, name = Credential.parse_name('name')
+        period, issuer, name = Credential.parse_long_name('name')
         self.assertEqual(30, period)
         self.assertEqual(None, issuer)
         self.assertEqual('name', name)
 
     def test_credential_serialize_name(self):
-        self.assertEqual('name', Credential.serialize_name('name'))
+        self.assertEqual('name', Credential('name').long_name())
         self.assertEqual(
-            'Issuer:name', Credential.serialize_name('name', issuer='Issuer'))
+            'Issuer:name', Credential('name', issuer='Issuer').long_name())
         self.assertEqual(
-            '20/Issuer:name', Credential.serialize_name(
-                'name', issuer='Issuer', period=20))
+            '20/Issuer:name', Credential(
+                'name', issuer='Issuer', period=20).long_name())
         self.assertEqual(
-            'Issuer:name', Credential.serialize_name(
-                'name', issuer='Issuer', period=30))
+            'Issuer:name', Credential(
+                'name', issuer='Issuer', period=30).long_name())
         self.assertEqual(
-            '20/name', Credential.serialize_name('name', period=20))
+            '20/name', Credential('name', period=20).long_name())
 
     def test_credential_expiration(self):
         cred = Credential('name')
