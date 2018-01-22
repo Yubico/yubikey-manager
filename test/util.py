f407a4bcbce0ce35fab56199073e4c8b10698170
Dag Heyman <dag@yubico.com>
2018-01-22 14:05:23 +0100

    Try to fix python 2

--- test/util.py
+++ test/util.py
@@ -6,7 +6,7 @@ from ykman.cli.__main__ import cli
 
 def ykman_cli(*argv, **kwargs):
     runner = CliRunner()
-    result = runner.invoke(cli, list(argv), **kwargs, obj={})
+    result = runner.invoke(cli, list(argv), obj={}, **kwargs)
     if result.exit_code != 0:
         click.echo(result.output)
         traceback.print_tb(result.exc_info[2])
