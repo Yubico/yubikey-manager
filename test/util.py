55cb5b6425c5d8e9bb620d7dc6f9da48a0cdab81
Emil Lundberg <emil@yubico.com>
2018-02-15 15:48:25 +0100

    Don't print stdout and traceback on test failure

--- test/util.py
+++ test/util.py
@@ -1,5 +1,3 @@
-import traceback
-import click
 from click.testing import CliRunner
 from ykman.cli.__main__ import cli
 
@@ -8,7 +6,5 @@ def ykman_cli(*argv, **kwargs):
     runner = CliRunner()
     result = runner.invoke(cli, list(argv), obj={}, **kwargs)
     if result.exit_code != 0:
-        click.echo(result.output)
-        traceback.print_tb(result.exc_info[2])
         raise result.exception
     return result.output
