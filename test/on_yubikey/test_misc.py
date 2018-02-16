7d8273ea73e8dd8fa89bebd9fc7cd775fec08e71
Emil Lundberg <emil@yubico.com>
2018-02-16 17:13:51 +0100

    Remove unnecessary sleep

--- test/on_yubikey/test_misc.py
+++ test/on_yubikey/test_misc.py
@@ -1,11 +1,9 @@
-import time
 from .util import (DestructiveYubikeyTestCase, ykman_cli)
 
 
 class TestYkmanInfo(DestructiveYubikeyTestCase):
 
     def test_ykman_info(self):
-        time.sleep(3)
         info = ykman_cli('info')
         self.assertIn('Device type:', info)
         self.assertIn('Serial number:', info)
