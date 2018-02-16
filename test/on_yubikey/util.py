a334d086ab03ccd5914080adaf0fd6f402f3980b
Emil Lundberg <emil@yubico.com>
2018-02-16 17:30:37 +0100

    Split module test.on_yubikey.piv out from test.test_cli_commands_on_yubikey

--- test/on_yubikey/util.py
+++ test/on_yubikey/util.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 import click
 import os
 import sys
-from cryptography.hazmat.primitives.asymmetric import ec, rsa, padding
 from ykman.descriptor import (
     get_descriptors, open_device, FailedOpeningDeviceException)
 from ykman.util import is_cve201715361_vulnerable_firmware_version
@@ -21,9 +20,6 @@ URI_TOTP_EXAMPLE_B = (
         'otpauth://totp/ACME%20Co:john.doe.b@email.com?'
         'secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co'
         '&algorithm=SHA1&digits=6&period=30')
-
-DEFAULT_MANAGEMENT_KEY = '010203040506070801020304050607080102030405060708'
-NON_DEFAULT_MANAGEMENT_KEY = '010103040506070801020304050607080102030405060708'
 
 _one_yubikey = False
 _the_yubikey = None
@@ -67,7 +63,7 @@ def _get_version():
     return _the_yubikey.version
 
 
-def _is_NEO():
+def is_NEO():
     if _one_yubikey:
         return _get_version() < (4, 0, 0)
     else:
@@ -93,20 +89,6 @@ def ykman_cli(*args, **kwargs):
         '--device', _test_serial,
         *args, **kwargs
     )
-
-
-def _verify_cert(cert, pubkey):
-    cert_signature = cert.signature
-    cert_bytes = cert.tbs_certificate_bytes
-
-    if isinstance(pubkey, rsa.RSAPublicKey):
-        pubkey.verify(cert_signature, cert_bytes, padding.PKCS1v15(),
-                      cert.signature_hash_algorithm)
-    elif isinstance(pubkey, ec.EllipticCurvePublicKey):
-        pubkey.verify(cert_signature, cert_bytes,
-                      ec.ECDSA(cert.signature_hash_algorithm))
-    else:
-        raise ValueError('Unsupported public key value')
 
 
 def missing_mode(transport):
