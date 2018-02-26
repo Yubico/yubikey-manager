8f3a5ba3dabc10849536c59924ff83239286dc71
Emil Lundberg <emil@yubico.com>
2018-02-26 12:19:46 +0100

    Split PIV tests into ones that require reset in setUp() and those that don't
    This saves a lot of time by reducing the amount of reset calls (each of
    which takes 1.5 seconds) needed.

--- test/on_yubikey/test_piv.py
+++ test/on_yubikey/test_piv.py
@@ -16,23 +16,9 @@ NON_DEFAULT_MANAGEMENT_KEY = '010103040506070801020304050607080102030405060708'
 
 @unittest.skipIf(*missing_mode(TRANSPORT.CCID))
 class PivTestCase(DestructiveYubikeyTestCase):
-    pass
-
-
-class ManagementKey(PivTestCase):
-
-    def setUp(self):
-        self.dev = open_device(transports=TRANSPORT.CCID)
-        self.controller = PivController(self.dev.driver)
-        self.controller.reset()
 
     def tearDown(self):
         self.dev.driver.close()
-
-    def reconnect(self):
-        self.dev.driver.close()
-        self.dev = open_device(transports=TRANSPORT.CCID)
-        self.controller = PivController(self.dev.driver)
 
     def assertMgmKeyIs(self, key):
         if type(key) is str:
@@ -56,14 +42,31 @@ class ManagementKey(PivTestCase):
             key = a2b_hex(key)
         self.assertNotEqual(self.controller._pivman_protected_data.key, key)
 
+    def reconnect(self):
+        self.dev.driver.close()
+        self.dev = open_device(transports=TRANSPORT.CCID)
+        self.controller = PivController(self.dev.driver)
+
+
+class ManagementKeyReadOnly(PivTestCase):
+    """
+    Tests after which the management key is always the default management key.
+    Placing compatible tests here reduces the amount of slow reset calls needed.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        dev = open_device(transports=TRANSPORT.CCID)
+        controller = PivController(dev.driver)
+        controller.reset()
+
+    def setUp(self):
+        self.dev = open_device(transports=TRANSPORT.CCID)
+        self.controller = PivController(self.dev.driver)
+
     def test_authenticate_twice_does_not_throw(self):
         self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
         self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
-
-    def test_reset_while_verified_throws_nice_ValueError(self):
-        self.controller.verify(DEFAULT_PIN)
-        with self.assertRaisesRegex(ValueError, '^Failed reading remaining'):
-            self.controller.reset()
 
     def test_reset_resets_has_stored_key_flag(self):
         self.assertFalse(self.controller.has_stored_key)
@@ -79,12 +82,10 @@ class ManagementKey(PivTestCase):
 
         self.assertFalse(self.controller.has_stored_key)
 
-    def test_set_mgm_key_changes_mgm_key(self):
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
-        self.controller.set_mgm_key(a2b_hex(NON_DEFAULT_MANAGEMENT_KEY))
-
-        self.assertMgmKeyIsNot(DEFAULT_MANAGEMENT_KEY)
-        self.assertMgmKeyIs(NON_DEFAULT_MANAGEMENT_KEY)
+    def test_reset_while_verified_throws_nice_ValueError(self):
+        self.controller.verify(DEFAULT_PIN)
+        with self.assertRaisesRegex(ValueError, '^Failed reading remaining'):
+            self.controller.reset()
 
     def test_set_mgm_key_does_not_change_key_if_not_authenticated(self):
         with self.assertRaises(APDUError):
@@ -97,6 +98,24 @@ class ManagementKey(PivTestCase):
             self.controller.set_mgm_key(None, store_on_device=True)
 
         self.assertMgmKeyIs(DEFAULT_MANAGEMENT_KEY)
+
+
+class ManagementKeyReadWrite(PivTestCase):
+    """
+    Tests after which the management key may not be the default management key.
+    """
+
+    def setUp(self):
+        self.dev = open_device(transports=TRANSPORT.CCID)
+        self.controller = PivController(self.dev.driver)
+        self.controller.reset()
+
+    def test_set_mgm_key_changes_mgm_key(self):
+        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
+        self.controller.set_mgm_key(a2b_hex(NON_DEFAULT_MANAGEMENT_KEY))
+
+        self.assertMgmKeyIsNot(DEFAULT_MANAGEMENT_KEY)
+        self.assertMgmKeyIs(NON_DEFAULT_MANAGEMENT_KEY)
 
     def test_set_stored_mgm_key_succeeds_if_pin_is_verified(self):
         self.controller.verify(DEFAULT_PIN)
