e6eeb0bc894236d08171c6833ba02cd8e75430e8
Emil Lundberg <emil@yubico.com>
2018-02-27 12:51:35 +0100

    Pre-unhexlify management key constants in test.on_yubikey.test_piv

--- test/on_yubikey/test_piv.py
+++ test/on_yubikey/test_piv.py
@@ -10,8 +10,8 @@ DEFAULT_PIN = '123456'
 NON_DEFAULT_PIN = '654321'
 DEFAULT_PUK = '12345678'
 NON_DEFAULT_PUK = '87654321'
-DEFAULT_MANAGEMENT_KEY = '010203040506070801020304050607080102030405060708'
-NON_DEFAULT_MANAGEMENT_KEY = '010103040506070801020304050607080102030405060708'
+DEFAULT_MANAGEMENT_KEY = a2b_hex('010203040506070801020304050607080102030405060708')  # noqa: E501
+NON_DEFAULT_MANAGEMENT_KEY = a2b_hex('010103040506070801020304050607080102030405060708')  # noqa: E501
 
 
 @unittest.skipIf(*missing_mode(TRANSPORT.CCID))
@@ -64,14 +64,14 @@ class ManagementKeyReadOnly(PivTestCase):
             PivController(dev.driver).reset()
 
     def test_authenticate_twice_does_not_throw(self):
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
 
     def test_reset_resets_has_stored_key_flag(self):
         self.assertFalse(self.controller.has_stored_key)
 
         self.controller.verify(DEFAULT_PIN)
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
         self.controller.set_mgm_key(None, store_on_device=True)
 
         self.assertTrue(self.controller.has_stored_key)
@@ -88,11 +88,11 @@ class ManagementKeyReadOnly(PivTestCase):
 
     def test_set_mgm_key_does_not_change_key_if_not_authenticated(self):
         with self.assertRaises(APDUError):
-            self.controller.set_mgm_key(a2b_hex(NON_DEFAULT_MANAGEMENT_KEY))
+            self.controller.set_mgm_key(NON_DEFAULT_MANAGEMENT_KEY)
         self.assertMgmKeyIs(DEFAULT_MANAGEMENT_KEY)
 
     def test_set_stored_mgm_key_does_not_destroy_key_if_pin_not_verified(self):
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
         with self.assertRaises(APDUError):
             self.controller.set_mgm_key(None, store_on_device=True)
 
@@ -109,16 +109,16 @@ class ManagementKeyReadWrite(PivTestCase):
         self.controller.reset()
 
     def test_set_mgm_key_changes_mgm_key(self):
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
-        self.controller.set_mgm_key(a2b_hex(NON_DEFAULT_MANAGEMENT_KEY))
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
+        self.controller.set_mgm_key(NON_DEFAULT_MANAGEMENT_KEY)
 
         self.assertMgmKeyIsNot(DEFAULT_MANAGEMENT_KEY)
         self.assertMgmKeyIs(NON_DEFAULT_MANAGEMENT_KEY)
 
     def test_set_stored_mgm_key_succeeds_if_pin_is_verified(self):
         self.controller.verify(DEFAULT_PIN)
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
-        self.controller.set_mgm_key(a2b_hex(NON_DEFAULT_MANAGEMENT_KEY),
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
+        self.controller.set_mgm_key(NON_DEFAULT_MANAGEMENT_KEY,
                                     store_on_device=True)
 
         self.assertMgmKeyIsNot(DEFAULT_MANAGEMENT_KEY)
@@ -128,7 +128,7 @@ class ManagementKeyReadWrite(PivTestCase):
 
     def test_set_stored_random_mgm_key_succeeds_if_pin_is_verified(self):
         self.controller.verify(DEFAULT_PIN)
-        self.controller.authenticate(a2b_hex(DEFAULT_MANAGEMENT_KEY))
+        self.controller.authenticate(DEFAULT_MANAGEMENT_KEY)
         self.controller.set_mgm_key(None, store_on_device=True)
 
         self.assertMgmKeyIsNot(DEFAULT_MANAGEMENT_KEY)
