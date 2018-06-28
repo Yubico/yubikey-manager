702ae6d725849b2f2598bd7a47dabe9eebb6d62c
Dag Heyman <dag@yubico.com>
2018-06-28 10:07:39 +0200

    piv: refactor algo validation

--- ykman/cli/piv.py
+++ ykman/cli/piv.py
@@ -217,9 +217,6 @@ def generate_key(
     PUBLIC-KEY  File containing the generated public key. Use '-' to use stdout.
     """
 
-    if ctx.obj['dev'].is_fips and algorithm == 'RSA1024':
-        ctx.fail('RSA1024 is not a supoported algorithm on YubiKey FIPS.')
-
     controller = ctx.obj['controller']
 
     _ensure_authenticated(ctx, controller, pin, management_key)
@@ -231,7 +228,7 @@ def generate_key(
     if touch_policy:
         touch_policy = TOUCH_POLICY.from_string(touch_policy)
 
-    _check_eccp384(ctx, controller, algorithm)
+    _check_algorithm(ctx, controller, algorithm)
     _check_pin_policy(ctx, controller, pin_policy)
     _check_touch_policy(ctx, controller, touch_policy)
 
@@ -344,9 +341,7 @@ def import_key(
 
     _check_pin_policy(ctx, controller, pin_policy)
     _check_touch_policy(ctx, controller, touch_policy)
-
-    if ctx.obj['dev'].is_fips and private_key.key_size == 1024:
-        ctx.fail('RSA1024 is not a supoported algorithm on YubiKey FIPS.')
+    _check_key_size(ctx, private_key)
 
     controller.import_key(
             slot,
@@ -799,10 +794,17 @@ def _authenticate(ctx, controller, management_key, mgm_key_prompt,
         ctx.fail('Authentication with management key failed.')
 
 
-def _check_eccp384(ctx, controller, algorithm):
+def _check_algorithm(ctx, controller, algorithm):
     #  ECCP384 not supported on NEO.
     if algorithm == ALGO.ECCP384 and controller.version < (4, 0, 0):
         ctx.fail('ECCP384 is not supported by this YubiKey.')
+    if algorithm == ALGO.RSA1024 and ctx.obj['dev'].is_fips:
+        ctx.fail('RSA1024 is not supoported by this YubiKey.')
+
+
+def _check_key_size(ctx, private_key):
+    if ctx.obj['dev'].is_fips and private_key.key_size == 1024:
+        ctx.fail('1024 is not a supoported key size on this YubiKey.')
 
 
 def _check_pin_policy(ctx, controller, pin_policy):
