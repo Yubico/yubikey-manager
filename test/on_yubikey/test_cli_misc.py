8762e5e9eb97a9213c423fd7c36d1096614b9837
Dag Heyman <dag@yubico.com>
2018-07-06 09:06:13 +0200

    Drop dead test

--- test/on_yubikey/test_cli_misc.py
+++ test/on_yubikey/test_cli_misc.py
@@ -11,11 +11,6 @@ class TestYkmanInfo(DestructiveYubikeyTestCase):
         self.assertIn('Serial number:', info)
         self.assertIn('Firmware version:', info)
 
-    @unittest.skipIf(not is_fips(), 'YubiKey FIPS required.')
-    def test_ykman_info_reports_fips_device(self):
-        info = ykman_cli('info')
-        self.assertIn('This YubiKey is capable of FIPS Approved Mode.', info)
-
     @unittest.skipIf(is_fips(), 'Not applicable to YubiKey FIPS.')
     def test_ykman_info_does_not_report_fips_for_non_fips_device(self):
         info = ykman_cli('info')
