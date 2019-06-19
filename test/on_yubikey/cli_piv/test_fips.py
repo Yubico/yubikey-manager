501f392dfc132176beb3c544086b790b382061a2
Emil Lundberg <emil@yubico.com>
2019-06-19 23:14:01 +0200

    Upgrade cli_piv/test_fips

--- test/on_yubikey/cli_piv/test_fips.py
+++ test/on_yubikey/cli_piv/test_fips.py
@@ -1,25 +1,29 @@
-import unittest
+from ykman.util import TRANSPORT
 from ...util import open_file
-from ..util import ykman_cli, is_fips
-from .util import PivTestCase
+from ..util import cli_test_suite, fips, DestructiveYubikeyTestCase
 
 
-@unittest.skipIf(not is_fips(), 'YubiKey FIPS required.')
-class TestFIPS(PivTestCase):
+@cli_test_suite(TRANSPORT.CCID)
+def additional_tests(ykman_cli):
+    class TestFIPS(DestructiveYubikeyTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        ykman_cli('piv', 'reset', '-f')
+        @classmethod
+        def setUpClass(cls):
+            ykman_cli('piv', 'reset', '-f')
 
-    @classmethod
-    def tearDownClass(cls):
-        ykman_cli('piv', 'reset', '-f')
+        @classmethod
+        def tearDownClass(cls):
+            ykman_cli('piv', 'reset', '-f')
 
-    def test_rsa1024_generate_blocked(self):
-        with self.assertRaises(SystemExit):
-            ykman_cli('piv', 'generate-key', '9a', '-a', 'RSA1024', '-')
+        @fips(True)
+        def test_rsa1024_generate_blocked(self):
+            with self.assertRaises(SystemExit):
+                ykman_cli('piv', 'generate-key', '9a', '-a', 'RSA1024', '-')
 
-    def test_rsa1024_import_blocked(self):
-        with self.assertRaises(SystemExit):
-            with open_file('rsa_1024_key.pem') as f:
-                ykman_cli('piv', 'import-key', '9a', f.name)
+        @fips(True)
+        def test_rsa1024_import_blocked(self):
+            with self.assertRaises(SystemExit):
+                with open_file('rsa_1024_key.pem') as f:
+                    ykman_cli('piv', 'import-key', '9a', f.name)
+
+    return [TestFIPS]
