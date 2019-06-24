88d6b204c7f45ee7b72e70628e527fbbae9f3664
Emil Lundberg <emil@yubico.com>
2019-06-24 17:24:02 +0200

    Upgrade test_cli_openpgp

--- test/on_yubikey/test_cli_openpgp.py
+++ test/on_yubikey/test_cli_openpgp.py
@@ -1,20 +1,22 @@
 import unittest
-from ykman.util import TRANSPORT
-from .util import (DestructiveYubikeyTestCase, missing_mode, ykman_cli)
+from .util import cli_test_suite
 
 
-@unittest.skipIf(*missing_mode(TRANSPORT.CCID))
-class TestOpenPGP(DestructiveYubikeyTestCase):
+@cli_test_suite
+def additional_tests(ykman_cli):
+    class TestOpenPGP(unittest.TestCase):
 
-    def setUp(self):
-        ykman_cli('openpgp', 'reset', '-f')
+        def setUp(self):
+            ykman_cli('openpgp', 'reset', '-f')
 
-    def test_openpgp_info(self):
-        output = ykman_cli('openpgp', 'info')
-        self.assertIn('OpenPGP version:', output)
+        def test_openpgp_info(self):
+            output = ykman_cli('openpgp', 'info')
+            self.assertIn('OpenPGP version:', output)
 
-    def test_openpgp_reset(self):
-        output = ykman_cli('openpgp', 'reset', '-f')
-        self.assertIn(
-            'Success! All data has been cleared and default PINs are set.',
-            output)
+        def test_openpgp_reset(self):
+            output = ykman_cli('openpgp', 'reset', '-f')
+            self.assertIn(
+                'Success! All data has been cleared and default PINs are set.',
+                output)
+
+    return [TestOpenPGP]
