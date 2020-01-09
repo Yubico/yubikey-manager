eb9de15994442b6ce03d8e490755164e030d6d04
Emil Lundberg <emil@yubico.com>
2020-01-09 21:46:17 +0100

    Add test of reading/writing PIV cert as raw object

--- test/on_yubikey/cli_piv/test_read_write_object.py
+++ test/on_yubikey/cli_piv/test_read_write_object.py
@@ -1,9 +1,12 @@
 import os
 import unittest
 
+from cryptography.hazmat.primitives import serialization
 from ..framework import cli_test_suite
-from ykman.piv import OBJ
+from ykman.piv import OBJ, SLOT, TAG
+from ykman.util import Tlv
 from .util import DEFAULT_MANAGEMENT_KEY
+from ...util import generate_self_signed_certificate
 
 
 @cli_test_suite
@@ -37,5 +40,33 @@ def additional_tests(ykman_cli):
             output2 = ykman_cli.with_bytes_output('piv', 'read-object',
                                                   hex(OBJ.AUTHENTICATION))
             self.assertEqual(output2, data)
+
+        def test_read_write_certificate_as_object(self):
+            with self.assertRaises(SystemExit):
+                ykman_cli('piv', 'read-object', hex(OBJ.AUTHENTICATION))
+
+            cert = generate_self_signed_certificate()
+            cert_bytes_der = cert.public_bytes(
+                encoding=serialization.Encoding.DER)
+
+            input_tlv = (
+                Tlv(TAG.CERTIFICATE, cert_bytes_der) +
+                Tlv(TAG.CERT_INFO, b'\0') +
+                Tlv(TAG.LRC, b'')
+            )
+
+            ykman_cli('piv', 'write-object', hex(OBJ.AUTHENTICATION), '-',
+                      '-m', DEFAULT_MANAGEMENT_KEY,
+                      input=input_tlv)
+
+            output1 = ykman_cli.with_bytes_output('piv', 'read-object',
+                                                  hex(OBJ.AUTHENTICATION))
+            output_cert_bytes = Tlv.parse_dict(output1)[TAG.CERTIFICATE]
+            self.assertEqual(output_cert_bytes, cert_bytes_der)
+
+            output2 = ykman_cli.with_bytes_output('piv', 'export-certificate',
+                                                  hex(SLOT.AUTHENTICATION), '-',
+                                                  '--format', 'DER')
+            self.assertEqual(output2, cert_bytes_der)
 
     return [ReadWriteObject]
