6f39f06cee5e43998213704b4d7244e99cb2bedd
Emil Lundberg <emil@yubico.com>
2020-01-10 16:58:54 +0100

    Test that piv read-object preserves ANSI escape codes
    Objects written might (accidentally?) contain such codes, so they
    should be preserved when read back out. For example, there's a 281 in
    10^12 chance that any six random bytes happen to make the escape code for
    red text colour.

--- test/on_yubikey/cli_piv/test_misc.py
+++ test/on_yubikey/cli_piv/test_misc.py
@@ -29,4 +29,18 @@ def additional_tests(ykman_cli):
                 data = ykman_cli('piv', 'read-object', '0x5f0001')
             self.assertEqual(data, 'test data')
 
+        def test_write_read_preserves_ansi_escapes(self):
+            red = b'\x00\x1b[31m'
+            blue = b'\x00\x1b[34m'
+            reset = b'\x00\x1b[0m'
+            data = (b'Hello, ' + red + b'red' + reset + b' and ' + blue
+                    + b'blue' + reset + b' world!')
+            ykman_cli(
+                'piv', 'write-object',
+                '-m', DEFAULT_MANAGEMENT_KEY, '0x5f0001',
+                '-', input=data)
+            output_data = ykman_cli.with_bytes_output(
+                'piv', 'read-object', '0x5f0001')
+            self.assertEqual(data, output_data)
+
     return [Misc]
