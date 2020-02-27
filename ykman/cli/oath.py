b7e18055bda5738b441c83d4de814aec2fffad46
Dag Heyman <dag@yubico.com>
2020-02-27 13:48:15 +0100

    oath: adjust error msg
    Co-Authored-By: Emil Lundberg <emil@yubico.com>

--- ykman/cli/oath.py
+++ ykman/cli/oath.py
@@ -372,7 +372,7 @@ def code(ctx, show_hidden, query, single):
         _error_multiple_hits(ctx, [cr for cr, c in creds])
 
     elif single and len(creds) == 0:
-        ctx.fail('No credential found.')
+        ctx.fail('No matching credential found.')
 
     if single and creds:
         click.echo(creds[0][1].value)
