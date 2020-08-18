233b9921703c0eb5c5a0cefc08015418e9387fdc
Dennis Fokin <Dennis.Fokin@yubico.com>
2020-08-18 07:57:35 +0200

    bio-enrollment

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ from setuptools import setup
 
 install_requires = [
     'six', 'pyscard', 'pyusb', 'click',
-    'cryptography', 'pyopenssl', 'fido2 >= 0.7'
+    'cryptography', 'pyopenssl',
+    'fido2 @ git+https://git@github.com/Yubico/python-fido2@bio-enrollment'
 ]
 if sys.version_info < (3, 4):
     install_requires.append('enum34')
