a222ddc8bc99a9880a8228ef83bb0f8f61bdee81
Dain Nilsson <dain@yubico.com>
2020-10-05 11:13:17 +0200

    Fix interface test.

--- test/on_yubikey/test_interfaces.py
+++ test/on_yubikey/test_interfaces.py
@@ -3,22 +3,33 @@ import unittest
 from .framework import DestructiveYubikeyTestCase, exactly_one_yubikey_present
 from yubikit.core import TRANSPORT
 from ykman.device import connect_to_device
+from time import sleep
 
 
 @unittest.skipIf(
     not exactly_one_yubikey_present(), "Exactly one YubiKey must be present."
 )
 class TestInterfaces(DestructiveYubikeyTestCase):
+    def try_connection(self, transport):
+        for _ in range(8):
+            try:
+                conn = connect_to_device(None, transport)[0]
+                conn.close()
+                return
+            except Exception:
+                sleep(0.5)
+        self.fail("Failed connecting to deivce over " + transport.name)
+
     def test_switch_interfaces(self):
-        connect_to_device(None, TRANSPORT.FIDO)
-        connect_to_device(None, TRANSPORT.OTP)
-        connect_to_device(None, TRANSPORT.FIDO)
-        connect_to_device(None, TRANSPORT.CCID)
-        connect_to_device(None, TRANSPORT.OTP)
-        connect_to_device(None, TRANSPORT.CCID)
-        connect_to_device(None, TRANSPORT.OTP)
-        connect_to_device(None, TRANSPORT.FIDO)
-        connect_to_device(None, TRANSPORT.CCID)
-        connect_to_device(None, TRANSPORT.FIDO)
-        connect_to_device(None, TRANSPORT.CCID)
-        connect_to_device(None, TRANSPORT.OTP)
+        self.try_connection(TRANSPORT.FIDO)
+        self.try_connection(TRANSPORT.OTP)
+        self.try_connection(TRANSPORT.FIDO)
+        self.try_connection(TRANSPORT.CCID)
+        self.try_connection(TRANSPORT.OTP)
+        self.try_connection(TRANSPORT.CCID)
+        self.try_connection(TRANSPORT.OTP)
+        self.try_connection(TRANSPORT.FIDO)
+        self.try_connection(TRANSPORT.CCID)
+        self.try_connection(TRANSPORT.FIDO)
+        self.try_connection(TRANSPORT.CCID)
+        self.try_connection(TRANSPORT.OTP)
