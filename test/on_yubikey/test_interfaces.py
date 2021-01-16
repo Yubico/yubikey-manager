1ef1851e508295f6d4bf01289591f42c21656df7
Dain Nilsson <dain@yubico.com>
2021-01-16 18:02:03 +0100

    Test handling of reclaim timeout.

--- test/on_yubikey/test_interfaces.py
+++ test/on_yubikey/test_interfaces.py
@@ -4,6 +4,7 @@ from .framework import DestructiveYubikeyTestCase, exactly_one_yubikey_present
 from yubikit.core.otp import OtpConnection
 from yubikit.core.fido import FidoConnection
 from yubikit.core.smartcard import SmartCardConnection
+from ykman.base import YUBIKEY
 from ykman.device import connect_to_device
 from time import sleep
 
@@ -13,14 +14,15 @@ from time import sleep
 )
 class TestInterfaces(DestructiveYubikeyTestCase):
     def try_connection(self, conn_type):
-        for _ in range(8):
-            try:
-                conn = connect_to_device(None, [conn_type])[0]
-                conn.close()
-                return
-            except Exception:
-                sleep(0.5)
-        self.fail("Failed connecting to device over %s" % conn_type)
+        if self.key_type == YUBIKEY.NEO and conn_type == SmartCardConnection:
+            sleep(3.5)
+        conn, dev, info = connect_to_device(None, [conn_type])
+        conn.close()
+
+    def setUp(self):
+        conn, dev, info = connect_to_device()
+        conn.close()
+        self.key_type = dev.pid.get_type()
 
     def test_switch_interfaces(self):
         self.try_connection(FidoConnection)
@@ -29,9 +31,4 @@ class TestInterfaces(DestructiveYubikeyTestCase):
         self.try_connection(SmartCardConnection)
         self.try_connection(OtpConnection)
         self.try_connection(SmartCardConnection)
-        self.try_connection(OtpConnection)
         self.try_connection(FidoConnection)
-        self.try_connection(SmartCardConnection)
-        self.try_connection(FidoConnection)
-        self.try_connection(SmartCardConnection)
-        self.try_connection(OtpConnection)
