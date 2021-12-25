a45797228a4ade996422a6dedd74ff69c19ede8f
Daniel Kilimnik <daniel@neodyme.io>
2021-12-25 11:33:23 +0100

    add change [Admin] PIN method/cli

--- tests/device/cli/test_openpgp.py
+++ tests/device/cli/test_openpgp.py
@@ -2,6 +2,15 @@ from yubikit.management import CAPABILITY
 from .. import condition
 import pytest
 
+DEFAULT_PIN = "123456"
+NON_DEFAULT_PIN = "654321"
+DEFAULT_ADMIN_PIN = "12345678"
+NON_DEFAULT_ADMIN_PIN = "87654321"
+
+
+def old_new_new(old, new):
+    return f"{old}\n{new}\n{new}\n"
+
 
 @pytest.fixture(autouse=True)
 @condition.capability(CAPABILITY.OPENPGP)
@@ -17,3 +26,63 @@ class TestOpenPGP:
     def test_openpgp_reset(self, ykman_cli):
         output = ykman_cli("openpgp", "reset", "-f").output
         assert "Success! All data has been cleared and default PINs are set." in output
+
+
+class TestPin:
+    def test_change_pin(self, ykman_cli):
+        ykman_cli(
+            "openpgp", "access", "change-pin", "-P", DEFAULT_PIN, "-n", NON_DEFAULT_PIN
+        )
+        ykman_cli(
+            "openpgp", "access", "change-pin", "-P", NON_DEFAULT_PIN, "-n", DEFAULT_PIN
+        )
+
+    def test_change_pin_prompt(self, ykman_cli):
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-pin",
+            input=old_new_new(DEFAULT_PIN, NON_DEFAULT_PIN),
+        )
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-pin",
+            input=old_new_new(NON_DEFAULT_PIN, DEFAULT_PIN),
+        )
+
+
+class TestAdminPin:
+    def test_change_admin_pin(self, ykman_cli):
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-admin-pin",
+            "-a",
+            DEFAULT_ADMIN_PIN,
+            "-n",
+            NON_DEFAULT_ADMIN_PIN,
+        )
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-admin-pin",
+            "-a",
+            NON_DEFAULT_ADMIN_PIN,
+            "-n",
+            DEFAULT_ADMIN_PIN,
+        )
+
+    def test_change_pin_prompt(self, ykman_cli):
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-admin-pin",
+            input=old_new_new(DEFAULT_ADMIN_PIN, NON_DEFAULT_ADMIN_PIN),
+        )
+        ykman_cli(
+            "openpgp",
+            "access",
+            "change-admin-pin",
+            input=old_new_new(NON_DEFAULT_ADMIN_PIN, DEFAULT_ADMIN_PIN),
+        )
