03a4e22659747f2708ceef8db0633eeceaf00f1a
Dain Nilsson <dain@yubico.com>
2022-03-07 11:07:36 +0100

    Handle get_uv_retries returning an int.

--- ykman/cli/fido.py
+++ ykman/cli/fido.py
@@ -122,7 +122,9 @@ def info(ctx):
 
         bio_enroll = ctap2.info.options.get("bioEnroll")
         if bio_enroll:
-            uv_retries, _ = client_pin.get_uv_retries()
+            uv_retries = client_pin.get_uv_retries()
+            if isinstance(uv_retries, tuple):
+                uv_retries = uv_retries[0]
             if uv_retries:
                 click.echo(
                     f"Fingerprints registered, with {uv_retries} attempt(s) "
