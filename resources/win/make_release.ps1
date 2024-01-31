07b7bd07e0366fbf134a0979a044b548bf45cf3f
Dain Nilsson <dain@yubico.com>
2024-01-31 13:10:34 +0100

    Switch to new Windows cert

--- resources/win/make_release.ps1
+++ resources/win/make_release.ps1
@@ -6,7 +6,7 @@ $CWD = pwd
 $SOURCE_DIR = "$CWD\ykman"
 
 echo "Signing ykman.exe"
-signtool.exe sign /sha1 DD86A2E1383B0E4E1C823B606DDBBCC26E1FF82D /fd SHA256 /t http://timestamp.digicert.com "$SOURCE_DIR\ykman.exe"
+signtool.exe sign /sha1 cb7cade8f51d0985d806bcd28e1fec57d3034187 /fd SHA256 /t http://timestamp.digicert.com "$SOURCE_DIR\ykman.exe"
 
 $VERSION = $(& "$SOURCE_DIR\ykman.exe" --version).Split(' ')[-1]
 
@@ -16,6 +16,6 @@ echo "Signing .msi"
 $OUTPUT_FILE = "yubikey-manager-$VERSION-win64.msi"
 mv ".\ykman.msi" $OUTPUT_FILE
 
-signtool.exe sign /sha1 DD86A2E1383B0E4E1C823B606DDBBCC26E1FF82D /fd SHA256 /t http://timestamp.digicert.com /d "YubiKey Manager CLI" ".\$OUTPUT_FILE"
+signtool.exe sign /sha1 cb7cade8f51d0985d806bcd28e1fec57d3034187 /fd SHA256 /t http://timestamp.digicert.com /d "YubiKey Manager CLI" ".\$OUTPUT_FILE"
 
 echo "Installer signed: $OUTPUT_FILE"
