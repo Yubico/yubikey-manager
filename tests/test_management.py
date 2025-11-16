46e977eaa7152ddabb08804b411bfa43acc91fef
Latif Noor <Noor.Latif@toyota-industries.eu>
2025-11-16 06:19:21 +0100

    tests: clarify management form factor coverage

--- tests/test_management.py
+++ tests/test_management.py
@@ -1,6 +1,8 @@
+import pytest
+
 from yubikit.management import FORM_FACTOR
 
 
-def test_form_factor_from_code():
-    for ff in FORM_FACTOR:
-        assert ff == FORM_FACTOR.from_code(ff)
+@pytest.mark.parametrize("form_factor", list(FORM_FACTOR))
+def test_form_factor_from_code_accepts_enum_members(form_factor):
+    assert form_factor == FORM_FACTOR.from_code(form_factor)
